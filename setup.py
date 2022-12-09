2bb9143dedbb4b9e2fef0570fcab70cf9e00f9ef
Jorge Salinas <jorgesalinas7140@gmail.com>
2022-12-09 15:07:40 -0500

    OFI-104-find-packages

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 __version__ = "0.1.0"
 
 setup(
    name="setup",
    version=__version__,
+    packages=(
+        find_packages() +
+        find_packages(where="./")
+    ),
 )
-
