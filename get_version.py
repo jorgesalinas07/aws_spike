d65b2c87fb2ce9e4a4dafebba96d84920ffb8346
Jorge Salinas <jorgesalinas7140@gmail.com>
2022-12-19 12:57:52 -0500

    feat: get_Version file

---
+++ get_version.py
@@ -0,0 +1,19 @@
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+if __name__=="__main__":
+    version = get_version("setup.py")
+    print(version)
